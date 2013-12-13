c842d16b0a9a581cc412f33ce9ffc66d35c9ff8a
Steve Pucci <spucci@google.com>
2013-12-13 14:03:24 -0800

    Change header to incorporate Todd's feedback
    Change-Id: I54e0a80c8f947c53e0fa57db092114ef2e7bfbb1

--- scripts/lldb-rld.pl
+++ scripts/lldb-rld.pl
@@ -1,10 +1,11 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 
 # This script performs Run Length Decoding on a packet encoded in the gdbserver RLE protocol.
 # This script is not particularly useful but I used it to be able to track the register-info
 # packet from gdbserver to lldb to ensure I was looking at exactly the right data.
 
 use strict;
+use warnings;
 
 my $inp = 'fcf*"a8e3cebe10*"0f*"ffc08a3c710*"00d48a3c71fc0*"088b3c71140*"d8c3cf411ce5cebea8e3cebe88e3cebeebc5114054470b4010* e20b80*"120*"8e0**360*"5c0*"750*"e0968e762e0069006e007000750074006d006500740068006f0064002e006c006100740*}0*>10*+70*H800040*;40*780*230*"030003030303070707072f0*"30*(10*"010*"010*"010* 80b30* 80b60* 40*)10*!60';
 
