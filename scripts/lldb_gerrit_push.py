84bf87e0cea15c6c139c6899e15be4ff9a22fea5
Steve Pucci <spucci@google.com>
2013-12-27 10:18:45 -0800

    Make gpylint-clean
    Change-Id: Id3aee9b6a0a7fe6393bc31a1724a6e01388b0671

--- scripts/lldb_gerrit_push.py
+++ scripts/lldb_gerrit_push.py
@@ -1,49 +1,55 @@
 #!/usr/bin/python
 
-# Pushes to the appropriate branch, as with
-#
-#    git push origin HEAD:refs/for/<branch>%r=<reviewer>@google.com
-#
-# by typing
-#
-#    lldb-gerrit-push.py <reviewer>  [<branch>]
-#
-# where <branch> defaults to "master"
+"""Pushes with git to the appropriate branch for the current directory.
 
-import sys
+Example:
+
+    lldb-gerrit-push.py <reviewer>  [<branch>]
+
+(where <branch> defaults to "master") will generate and execute the following
+command:
+
+    git push origin HEAD:refs/for/<branch>%r=<reviewer>@google.com
+
+"""
+
 import os
+import sys
 
-def usage_and_die():
-    print "Usage:  lldb-gerrit-push.py  <reviewer>  [<branch>]"
-    exit(1)
 
-# TODO: Check that current sandbox is properly a branch with a single unpushed commit
+def UsageAndDie():
+  print "Usage:  lldb-gerrit-push.py  <reviewer>  [<branch>]"
+  exit(1)
 
-# TODO: Store/restore reviewer for this branch/repository?
+
+# TODO(spucci): Check that current sandbox is properly a branch with a
+#               single unpushed commit
+
+# TODO(spucci): Store/restore reviewer for this branch/repository?
 
 if len(sys.argv) < 2:
-    print "Need reviewer!"
-    usage_and_die()
+  print "Need reviewer!"
+  UsageAndDie()
 
 reviewer = sys.argv[1]
 if not reviewer.endswith("@google.com"):
-    if (reviewer.find("@") >= 0):
-        print "Reviewer should be @google.com address, not", reviewer
-        usage_and_die()
-    reviewer += "@google.com"
-#print "Reviewer: ", reviewer
+  if reviewer.find("@") >= 0:
+    print "Reviewer should be @google.com address, not", reviewer
+    UsageAndDie()
+  reviewer += "@google.com"
+# print "Reviewer: ", reviewer
 
-# TODO: Add check that reviewer is valid google ldap
-# TODO: (optionally) add cc=lldb@google.com
+# TODO(spucci): Add check that reviewer is valid google ldap
+# TODO(spucci): (optionally) add cc=lldb@google.com
 
 if len(sys.argv) < 3:
-    # TODO: Determine appropriate branch name from current sandbox
-    # No branch, assume master
-    branch = "master"
+  # TODO(spucci): Determine appropriate branch name from current sandbox
+  # No branch, assume master
+  branch = "master"
 else:
-    branch = sys.argv[2]
+  branch = sys.argv[2]
 
-#print "Branch: ", branch
+# print "Branch: ", branch
 
 command = "git push origin HEAD:refs/for/" + branch + "%r=" + reviewer
 
