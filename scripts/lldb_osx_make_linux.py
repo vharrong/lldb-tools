a97449e7a842547829e47475c76caa4f38e137c4
Todd Fiala <tfiala@google.com>
2014-05-08 15:43:02 -0700

    Added script that syncs a MacOSX-hosted lldb source tree over to linux and builds it.
    Change-Id: Ie06b2e5fbdcecacbf0d7ac6e728ea37475f3c441

---
+++ scripts/lldb_osx_make_linux.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os.path
+import re
+import subprocess
+
+_USER = "tfiala"
+
+_COMMON_SYNC_OPTS = "-avzhe ssh"
+_COMMON_EXCLUDE_OPTS = "--exclude=DerivedData --exclude=.svn --exclude=.git --exclude=llvm-build/Release+Asserts"
+
+_LINUX_SYNC_ROOT_PATH = "lldb/macosx.sync"
+_LOCAL_SYNC_LLDB_PATH = "~/lldb/git/s01/lldb"
+
+_REMOTE_HOSTNAME = "tfiala2.mtv.corp.google.com"
+
+
+def sync_llvm():
+    commandline = ['rsync']
+    commandline.extend(_COMMON_SYNC_OPTS.split())
+    commandline.extend(_COMMON_EXCLUDE_OPTS.split())
+    commandline.extend([
+        "%s/llvm" % os.path.expanduser(_LOCAL_SYNC_LLDB_PATH),
+        "%s@%s:%s" % (_USER, _REMOTE_HOSTNAME, _LINUX_SYNC_ROOT_PATH)])
+    return subprocess.call(commandline)
+
+
+def sync_lldb():
+    commandline = ['rsync']
+    commandline.extend(_COMMON_SYNC_OPTS.split())
+    commandline.extend(_COMMON_EXCLUDE_OPTS.split())
+    commandline.extend([
+        "--exclude=/lldb/llvm",
+        os.path.expanduser(_LOCAL_SYNC_LLDB_PATH),
+        "%s@%s:%s/llvm/tools" % (_USER, _REMOTE_HOSTNAME, _LINUX_SYNC_ROOT_PATH)])
+    return subprocess.call(commandline)
+
+
+def maybe_configure():
+    commandline = [
+        "ssh",
+        "%s@%s" % (_USER, _REMOTE_HOSTNAME),
+        "cd",
+        _LINUX_SYNC_ROOT_PATH,
+        "&&",
+        "touch",
+        "llvm/.git",
+        "&&",
+        "lldb_configure.py",
+        "-c"]
+    return subprocess.call(commandline)
+
+
+def build():
+    commandline = [
+        "ssh",
+        "%s@%s" % (_USER, _REMOTE_HOSTNAME),
+        "cd",
+        "%s/build" % _LINUX_SYNC_ROOT_PATH,
+        "&&",
+        "time",
+        "ninja"]
+    return subprocess.call(commandline)
+
+
+if __name__ == "__main__":
+    sync_llvm()
+    sync_lldb()
+    maybe_configure()
+    build()
