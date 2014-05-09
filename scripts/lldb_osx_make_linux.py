f6035e67fabe0f570185029a2a1b66337c2f2e4f
Todd Fiala <tfiala@google.com>
2014-05-09 15:21:49 -0700

    Added command-line/dotfile arg parsing; stderr handing; clean operation.
    Change-Id: I0a23828ce8699cd24ffe58923ceb1a12ee43af49

--- scripts/lldb_osx_make_linux.py
+++ scripts/lldb_osx_make_linux.py
@@ -1,53 +1,120 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+from __future__ import print_function
+
+import argparse
 import getpass
 import os
 import os.path
 import re
+import select
+import sys
 import subprocess
-
-_USER = getpass.getuser()
 
 _COMMON_SYNC_OPTS = "-avzhe ssh"
 _COMMON_EXCLUDE_OPTS = "--exclude=DerivedData --exclude=.svn --exclude=.git --exclude=llvm-build/Release+Asserts"
 
-_LINUX_SYNC_ROOT_PATH = "/mnt/ssd/work/macosx.sync"
-_LOCAL_SYNC_LLDB_PATH = os.getcwd()
+# _LOCAL_SYNC_LLDB_PATH = os.getcwd()
 
-_REMOTE_HOSTNAME = "tfiala2.mtv.corp.google.com"
+def parse_args():
+    DEFAULT_REMOTE_ROOT_DIR = "/mnt/ssd/work/macosx.sync"
+    DEFAULT_REMOTE_HOSTNAME = "tfiala2.mtv.corp.google.com"
+    OPTIONS_FILENAME = '.osx-linux-build.conf'
+
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+
+    parser.add_argument(
+        "--local-lldb-dir", "-l", metavar="DIR",
+        help="specify local lldb directory (Xcode layout assumed for llvm/clang)",
+        default=os.getcwd())
+    parser.add_argument(
+        "--remote-address", "-r", metavar="REMOTE-ADDR",
+        help="specify the dns name or ip address of the remote linux system",
+        default=DEFAULT_REMOTE_HOSTNAME)
+    parser.add_argument(
+        "--remote-dir", "-d", metavar="DIR",
+        help="specify the root of the linux source/build dir",
+        default=DEFAULT_REMOTE_ROOT_DIR)
+    parser.add_argument(
+        "--user", "-u", help="specify the user name for the remote system",
+        default=getpass.getuser())
+    parser.add_argument(
+        "--xcode-action", "-x", help="$(ACTION) from Xcode", nargs='?', default=None)
+
+    command_line_args = sys.argv[1:]
+    if os.path.exists(OPTIONS_FILENAME):
+        # Prepend the file so that command line args override the file contents.
+        command_line_args.insert(0, "@%s" % OPTIONS_FILENAME)
+
+    return parser.parse_args(command_line_args)
 
 
-_LLDB_DIR_RELATIVE_REGEX = re.compile("%s/llvm/tools/lldb/" % _LINUX_SYNC_ROOT_PATH)
-_LLVM_DIR_RELATIVE_REGEX = re.compile("%s/" % _LINUX_SYNC_ROOT_PATH)
+def maybe_create_remote_root_dir(args):
+    commandline = [
+        "ssh",
+        "%s@%s" % (args.user, args.remote_address),
+        "mkdir",
+        "-p",
+        args.remote_dir]
+    return subprocess.call(commandline)
 
 
-def sync_llvm():
+def init_with_args(args):
+    args.local_lldb_dir = os.path.expanduser(args.local_lldb_dir)
+
+    # We assume the local lldb directory is really named 'lldb'.
+    # This is because on the remote end, the local lldb root dir
+    # is copied over underneath llvm/tools and will be named there
+    # whatever it is named locally.  The remote build will assume
+    # is is called lldb.
+    if os.path.basename(args.local_lldb_dir) != 'lldb':
+        raise Exception(
+            "local lldb root needs to be called 'lldb' but was {} instead"
+            .format(os.path.basename(args.local_lldb_dir)))
+
+    args.lldb_dir_relative_regex = re.compile("%s/llvm/tools/lldb/" % args.remote_dir)
+    args.llvm_dir_relative_regex = re.compile("%s/" % args.remote_dir)
+
+    print("Xcode action:", args.xcode_action)
+
+    # Ensure the remote directory exists.
+    result = maybe_create_remote_root_dir(args)
+    if result == 0:
+        print("using remote root dir: %s" % args.remote_dir)
+    else:
+        print("remote root dir doesn't exist and could not be created, "
+              + "error code:", result)
+        return False
+
+    return True
+
+def sync_llvm(args):
     commandline = ['rsync']
     commandline.extend(_COMMON_SYNC_OPTS.split())
     commandline.extend(_COMMON_EXCLUDE_OPTS.split())
     commandline.extend([
-        "%s/llvm" % os.path.expanduser(_LOCAL_SYNC_LLDB_PATH),
-        "%s@%s:%s" % (_USER, _REMOTE_HOSTNAME, _LINUX_SYNC_ROOT_PATH)])
+        "%s/llvm" % args.local_lldb_dir,
+        "%s@%s:%s" % (args.user, args.remote_address, args.remote_dir)])
     return subprocess.call(commandline)
 
 
-def sync_lldb():
+def sync_lldb(args):
     commandline = ['rsync']
     commandline.extend(_COMMON_SYNC_OPTS.split())
     commandline.extend(_COMMON_EXCLUDE_OPTS.split())
     commandline.extend([
         "--exclude=/lldb/llvm",
-        os.path.expanduser(_LOCAL_SYNC_LLDB_PATH),
-        "%s@%s:%s/llvm/tools" % (_USER, _REMOTE_HOSTNAME, _LINUX_SYNC_ROOT_PATH)])
+        args.local_lldb_dir,
+        "%s@%s:%s/llvm/tools" % (args.user, args.remote_address, args.remote_dir)])
     return subprocess.call(commandline)
 
 
-def maybe_configure():
+def maybe_configure(args):
     commandline = [
         "ssh",
-        "%s@%s" % (_USER, _REMOTE_HOSTNAME),
+        "%s@%s" % (args.user, args.remote_address),
         "cd",
-        _LINUX_SYNC_ROOT_PATH,
+        args.remote_dir,
         "&&",
         "touch",
         "llvm/.git",
@@ -57,39 +124,104 @@ def maybe_configure():
     return subprocess.call(commandline)
 
 
-def filter_build_line(line):
-    lldb_relative_line = _LLDB_DIR_RELATIVE_REGEX.sub('', line)
+def filter_build_line(args, line):
+    lldb_relative_line = args.lldb_dir_relative_regex.sub('', line)
     if len(lldb_relative_line) != len(line):
         # We substituted - return the modified line
         return lldb_relative_line
 
     # No match on lldb path (longer on linux than llvm path).  Try
     # the llvm path match.
-    return _LLVM_DIR_RELATIVE_REGEX.sub('', line)
+    return args.llvm_dir_relative_regex.sub('', line)
 
 
-def build():
+def build(args):
     commandline = [
         "ssh",
-        "%s@%s" % (_USER, _REMOTE_HOSTNAME),
+        "%s@%s" % (args.user, args.remote_address),
         "cd",
-        "%s/build" % _LINUX_SYNC_ROOT_PATH,
+        "%s/build" % args.remote_dir,
         "&&",
         "time",
         "ninja"]
-    proc = subprocess.Popen(commandline, stdout=subprocess.PIPE)
+    proc = subprocess.Popen(
+        commandline,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+    # Filter stdout/stderr output for file path mapping.
+    # We do this to enable Xcode to see filenames relative to the
+    # MacOSX-side directory structure.
     while True:
-        line = proc.stdout.readline()
-        if line != '':
-            #the real code does filtering here
-            print filter_build_line(line)
-        else:
-            break
-    return None
+        reads = [proc.stdout.fileno(), proc.stderr.fileno()]
+        select_result = select.select(reads, [], [])
+
+        for fd in select_result[0]:
+            if fd == proc.stdout.fileno():
+                line = proc.stdout.readline()
+                print(filter_build_line(args, line.rstrip()))
+            elif fd == proc.stderr.fileno():
+                line = proc.stderr.readline()
+                print(filter_build_line(args, line.rstrip()), file=sys.stderr)
+
+        proc_retval = proc.poll()
+        if proc_retval != None:
+            return proc_retval
+
+
+def clean(args):
+    commandline = [
+        "ssh",
+        "%s@%s" % (args.user, args.remote_address),
+        "cd",
+        "%s/build" % args.remote_dir,
+        "&&",
+        "time",
+        "ninja",
+        "clean"]
+    proc = subprocess.Popen(
+        commandline,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+    # Filter stdout/stderr output for file path mapping.
+    # We do this to enable Xcode to see filenames relative to the
+    # MacOSX-side directory structure.
+    while True:
+        reads = [proc.stdout.fileno(), proc.stderr.fileno()]
+        select_result = select.select(reads, [], [])
+
+        for fd in select_result[0]:
+            if fd == proc.stdout.fileno():
+                line = proc.stdout.readline()
+                print(filter_build_line(args, line.rstrip()))
+            elif fd == proc.stderr.fileno():
+                line = proc.stderr.readline()
+                print(filter_build_line(args, line.rstrip()), file=sys.stderr)
+
+        proc_retval = proc.poll()
+        if proc_retval != None:
+            return proc_retval
 
 
 if __name__ == "__main__":
-    sync_llvm()
-    sync_lldb()
-    maybe_configure()
-    build()
+    # Handle arg parsing.
+    args = parse_args()
+
+    # Initialize the system.
+    if not init_with_args(args):
+        exit(1)
+
+    # Sync over llvm and clang source.
+    sync_llvm(args)
+
+    # Sync over lldb source.
+    sync_lldb(args)
+
+    # Configure the remote build if it's not already.
+    maybe_configure(args)
+
+    if args.xcode_action == 'clean':
+        exit(clean(args))
+    else:
+        exit(build(args))
