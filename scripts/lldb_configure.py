7422e11612ff0b73011a75f033217b5e6bd1291b
Andy Chien <achien@blueshiftinc.com>
2014-07-22 14:53:03 -0700

    updated lldb_configure script

--- scripts/lldb_configure.py
+++ scripts/lldb_configure.py
@@ -39,32 +39,59 @@ def ParseCommandLine():
       description="Configure lldb build environment.")
 
   parser.add_argument(
-      "--cmake", "-c", action="store_true", dest="use_cmake",
+      "-a", "--enable-assertions", action="store_true", dest="enable_assertions",
+      help="enable assert() runtime checks (default: disabled)")
+  parser.add_argument(
+      "-o", "--enable-optimized", action="store_true", dest="enable_optimized",
+      help="generate optimized code (default: disabled)")
+  parser.add_argument(
+      "-s", "--debug-symbols", action="store_true", dest="enable_symbols",
+      help="generate debug symbols (default: disabled)")
+  parser.add_argument(
+      "-c", "--cmake", action="store_true", dest="use_cmake",
       help="set configure system to cmake (default: configure)")
   parser.add_argument(
-      "--build-dir", "-b", action="store", default="build",
+      "-n", "--ninja", action="store_true", dest="use_ninja",
+      help="set build tool to ninja (default: make)")
+  parser.add_argument(
+      "-l", "--clang", action="store_true", dest="use_clang",
+      help="set compiler to clang (default: gcc)")
+  parser.add_argument(
+      "-g", "--gold", action="store_true", dest="use_gold",
+      help="set linker to gold (default: gnu ld)")
+  parser.add_argument(
+      "--ccache", action="store_true", dest="use_ccache",
+      help="enable cached compiling (default: do not cache obj files)")
+  parser.add_argument(
+      "-u", "--unity", action="store_true", dest="use_unity",
+      help="enable unity build compiles (default: one src file per obj file)")
+  parser.add_argument(
+      "-k", "--incremental-link", action="store_true", dest="use_inc_link",
+      help="enable incremental linking with gold linker (default: no incremental linking)")
+  parser.add_argument(
+      "-b", "--build-dir", action="store",  dest="build_dir", default="build",
       help="specify the build dir, default: build")
   parser.add_argument(
-      "--coverage", action="store_true",
+      "-d", "--distcc", action="store_true", dest="use_distcc",
+      help="enable distributed compiles to a build farm (default: compile locally)")
+  parser.add_argument(
+      "--coverage", action="store_true", dest="coverage",
       help="enable code coverage capture during exe runs. Default: no capture")
   parser.add_argument(
-      "--install-dir", "-i", action="store", default="install",
+      "-i", "--install-dir", action="store",  dest="install_dir", default="install",
       help="specify the install dir, default: install")
-  parser.add_argument(
-      "--release", "-r", action="store_true",
-      help="enable a release build. Default: (debug)")
-  parser.add_argument(
-      "--release-debug", action="store_true",
-      help="enable a release build with debug info. Default: (debug)")
-  parser.add_argument(
-      "--stock-libedit", action="store_true",
-      help="use stock system libedit/libedit-dev. Default: use lldb-specific "
-      "libedit")
   parser.add_argument(
       "--with-python-dir",
       action="store",
+      
       default=os.environ.get("LLDB_PYTHON_BASE_DIR"),
       help="specify alternate python root dir")
+  parser.add_argument(
+      "-x", "--IDE-xcode", action="store_true", dest="ide_xcode",
+      help="create project files for Xcode IDE (default: do not create Xcode IDE project)")
+  parser.add_argument(
+      "-v", "--IDE-visual-studio", action="store_true", dest="ide_visual_studio",
+      help="create project files for MS Visual Studio IDE (default: do not create VS IDE project)")
 
   return parser.parse_args()
 
@@ -80,24 +107,51 @@ def AddFileForFindInProject(llvm_parent_dir):
   f.close()
 
 
-def GetCxxFlags(args, libedit_include_dir):
+def GetToolNames(args):
+  """Construct names of the shell tools (config, make, cc, cpp, and linker) required for the given options.
+
+  Args:
+    args: the results from parsing the command line.
+
+  Returns:
+    The shell tool names required for the given options.
+
+  """
+  
+  class Tool_Names: pass
+
+  tool_names = Tool_Names()
+  tool_names.config = ("configure" if not args.use_cmake else "cmake")
+  tool_names.make = ("make" if not args.use_ninja else "ninja")
+  tool_names.cc = ("gcc" if not args.use_clang else "clang")
+  tool_names.cxx = ("g++" if not args.use_clang else "clang++")
+  tool_names.ld = ("ld" if not args.use_gold else "ld.gold")
+
+  # add distcc prefix if enabled
+  if args.use_distcc:
+    tool_names.cc = "distcc " + tool_names.cc
+    tool_names.cxx = "distcc " + tool_names.cxx
+
+  # ccache should be the initial compiler prefix (before distcc),
+  # so all subsequent compile operations are skipped on a cache hit
+  if args.use_ccache:
+    tool_names.cc = "ccache " + tool_names.cc
+    tool_names.cxx = "ccache " + tool_names.cxx
+
+  return tool_names
+
+def GetCxxFlags(args):
   """Construct C++ compiler flags required for the given options.
 
   Args:
     args: the results from parsing the command line.
 
-    libedit_include_dir: the location where libedit include files
-      should come from when not using the stock libedit.
-
   Returns:
     The C++ compiler flags required for the given options.
 
   """
-  if args.stock_libedit:
-    flags = ""
-  else:
-    flags = "-I%s" % libedit_include_dir
-
+  flags = ""
+  
   if args.coverage:
     # add code coverage flags
     flags += " -fprofile-arcs -ftest-coverage"
@@ -109,23 +163,17 @@ def GetCxxFlags(args, libedit_include_dir):
   return flags
 
 
-def GetLdFlags(args, libedit_lib_dir):
+def GetLdFlags(args):
   """Construct linker flags required for the given options.
 
   Args:
     args: the results from parsing the command line.
 
-    libedit_lib_dir: the location where libedit lib files
-      should come from when not using the stock libedit.
-
   Returns:
     The C++ compiler flags required for the given options.
 
   """
-  if args.stock_libedit:
-    flags = ""
-  else:
-    flags = "-L%s" % libedit_lib_dir
+  flags = ""
 
   if args.coverage:
     # add code coverage flags
@@ -152,18 +200,7 @@ def main():
     print "Error: No llvm directory found in parent chain."
     exit(1)
 
-  # Do this before checking for clang so it will work even if if that
-  # fails (e.g., on a Mac)
   AddFileForFindInProject(llvm_parent_dir)
-
-  # fail if there is a clang in the path
-  clang_path = lldb_utils.FindInExecutablePath("clang")
-  if clang_path:
-    print "Error: 'clang' was found in PATH: " + clang_path
-    print "Our lldb build setup does not support building with clang."
-    print "Please remove clang from your path."
-    exit(1)
-
   build_dir = os.path.join(llvm_parent_dir, args.build_dir)
   install_dir = os.path.join(llvm_parent_dir, args.install_dir)
 
@@ -183,99 +220,90 @@ def main():
 
   scripts_dir = os.path.dirname(os.path.realpath(__file__))
   lldb_dir = os.path.dirname(scripts_dir)
-  local_libedit_dir = os.path.abspath(os.path.join(lldb_dir, "libedit"))
-  local_libedit_include_dir = os.path.join(local_libedit_dir, "include")
-  local_libedit_lib_dir = os.path.join(local_libedit_dir,
-                                       lldb_utils.FullPlatformName(), "lib")
-
-  if not os.path.exists(local_libedit_lib_dir):
-    print("Warning: libedit lib directory for platform does not exist:\n  " +
-          local_libedit_lib_dir)
-    print "Forcing use of stock libedit"
-    args.stock_libedit = True
-  else:
-    # FIXME: address LD_LIBRARY_PATH analog for Windows
-    if "LD_LIBRARY_PATH" in os.environ:
-      os.environ["LD_LIBRARY_PATH"] = (local_libedit_lib_dir +
-                                       ":" + os.environ["LD_LIBRARY_PATH"])
-    else:
-      os.environ["LD_LIBRARY_PATH"] = local_libedit_lib_dir
 
   # if code coverage is enabled, we must have debug info, either by
   # --release-debug or debug (i.e. release must not be set)
-  if args.coverage and args.release:
+  if args.coverage and not args.enable_symbols:
     print("Error: code coverage requires debug info in the build.\n"
-          "Choose --release-debug or exclude --release on the command line.")
+          "Add --debug-symbols to the command line.")
     exit(1)
+
+  # get names of config, make, compilers, & linker
+  tool_names = GetToolNames(args)
+
+  # get flag arguments for the compiler and linker
+  cxx_flags = GetCxxFlags(args)
+  ld_flags = GetLdFlags(args)
+
 
   # Make build directory
   os.makedirs(build_dir)
 
   with workingdir.WorkingDir(build_dir):
 
-    cxx_flags = GetCxxFlags(args, local_libedit_include_dir)
-    ld_flags = GetLdFlags(args, local_libedit_lib_dir)
+    build_type_name = ""
+    if args.enable_symbols and args.enable_assertions:
+      build_type_name = "Debug"
+    if args.enable_symbols and args.enable_optimized:
+      build_type_name = "RelWithDebInfo"
+    elif args.enable_optimized:
+      build_type_name = "Release"
+      
+    config_message = "configured for " + tool_names.config + "/" + tool_names.make + " (%s)" % build_type_name
+
+    if args.enable_symbols:
+      # we need to add -g to tell it to include debug info in the
+      # release build
+      cxx_flags += " -g"
 
     if args.use_cmake:
-      if args.release_debug:
-        build_type_name = "RelWithDebInfo"
-      elif args.release:
-        build_type_name = "Release"
-      else:
-        build_type_name = "Debug"
-      config_message = "configured for cmake/ninja (%s)" % build_type_name
-
       command_tokens = ("cmake",
-                        "-GNinja",
-                        "-DCMAKE_CXX_COMPILER=g++",
-                        "-DCMAKE_C_COMPILER=gcc",
-                        "-DCMAKE_CXX_FLAGS=%s" % cxx_flags,
-                        "-DCMAKE_SHARED_LINKER_FLAGS=%s" % ld_flags,
-                        "-DCMAKE_EXE_LINKER_FLAGS=%s" % ld_flags,
-                        "-DCMAKE_INSTALL_PREFIX:PATH=%s" % install_dir,
-                        "-DCMAKE_BUILD_TYPE=%s" % build_type_name,
+                        ("" if not args.use_ninja else "-GNinja"),
+                        "-DCMAKE_LINKER=" + tool_names.ld,
+                        "-DCMAKE_CXX_FLAGS=" + cxx_flags,
+                        "-DCMAKE_SHARED_LINKER_FLAGS=" + ld_flags,
+                        "-DCMAKE_EXE_LINKER_FLAGS=" + ld_flags,
+                        "-DCMAKE_INSTALL_PREFIX:PATH=" + install_dir,
+                        "-DCMAKE_BUILD_TYPE=" + build_type_name,
                         # Do not include this next flag if you want to see
                         # cmake maintainer-related messages.
                         "-Wno-dev",
                         os.path.join("..", "llvm"))
+
     else:
       command_tokens = [os.path.join("..", "llvm", "configure"),
                         "--enable-cxx11",
                         "--prefix=%s" % install_dir]
 
-      if args.release_debug:
+      if args.enable_optimized:
         command_tokens.append("--enable-optimized")
-        command_tokens.append("--enable-assertions")
-        # we need to add -g to tell it to include debug info in the
-        # release build
-        cxx_flags += " -g"
-        config_message = ("configured for configure/(g)make "
-                          "release,debuginfo,assertions)")
-      elif args.release:
-        command_tokens.append("--enable-optimized")
-        command_tokens.append("--disable-assertions")
-        config_message = "configured for configure/(g)make (release)"
       else:
         command_tokens.append("--disable-optimized")
+
+      if args.enable_assertions:
         command_tokens.append("--enable-assertions")
-        config_message = "configured for configure/(g)make (debug,assertions)"
+      else:
+        command_tokens.append("--disable-assertions")
 
       command_tokens.append("--with-extra-options=%s" % cxx_flags)
       command_tokens.append("--with-extra-ld-options=%s" % ld_flags)
 
-    print " ".join(command_tokens)
-    status = subprocess.call(command_tokens)
+    env_vars = os.environ
+    env_vars["CC"] = tool_names.cc
+    env_vars["CXX"] = tool_names.cxx
+    env_vars["DISTCC_HOSTS"] = ""
+    if args.use_ccache and args.use_clang:
+      env_vars["CCACHE_CPP2"] = "yes"  # This is intended to silence preproc -I warnings, but is not working as expected
+
+    print "CC='" + tool_names.cc + "' CXX='" + tool_names.cxx + "' " + " ".join(command_tokens)
+    status = subprocess.call(command_tokens, env=env_vars)
+#    status = 0;
     if status != 0:
       print "configure command failed (see above)."
       exit(1)
 
     print ""
     print config_message
-    if args.stock_libedit:
-      print "using stock system libedit"
-    else:
-      print "using custom libedit ({},{})".format(
-          local_libedit_include_dir, local_libedit_lib_dir)
 
     if args.with_python_dir:
       print "using custom python ({})".format(args.with_python_dir)
