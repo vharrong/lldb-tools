2d681bc8e72899a3f07c1f7d4ac987b2a69e0e5c
Vince Harron <vharron@google.com>
2015-04-01 00:24:15 -0700

    Merged mk-trusty-armhf.sh and mk-precise-armel.sh into mk-ubuntu-img.sh

--- scripts/mk-ubuntu-img.sh
+++ scripts/mk-ubuntu-img.sh
@@ -1,27 +1,98 @@
 #!/bin/bash -ex
+# This script creates an qemu ubuntu image for a specific ubuntu release and arch
+# it also creates a qemu binary that can run it
+# if you're already created the image, you can use this script to launch it
+
+# Usage:
+# ./mk-ubuntu-img.sh <ubuntu-release> <ubuntu-arch>
+
+# Tested configurations:
+# ./mk-ubuntu-img.sh trusty arm64
+# ./mk-ubuntu-img.sh trusty armhf
+# ./mk-ubuntu-img.sh precise armel
+
+# Script dependencies:
+# sudo apt-get install debootstrap qemu-utils qemu
+
+# Install build deps for qemu (apt-src must be enabled in sources.list)
+# sudo apt-get build-dep qemu
 
 # TODO
-# remove exports?
 # build outside of source directories
+# add default user to sudoers
 
+UBUNTU_RELEASE=$1 # trusty or precise
+UBUNTU_ARCH=$2 # armel (precise only), armhf or arm64
+VMHOME="$HOME/vm"
+NCPU=`egrep -c ^processor /proc/cpuinfo`
+
+# passwordless ssh setup is disabled because I can't get host->guest connections working
 if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
   echo "Please run this command first:"
   echo "yes '' | ssh-keygen -t rsa"
 fi
 
 # configuration
-export ARCH=arm
-export CROSS_COMPILE=arm-none-eabi-
-ARCHEX=armel
-RELEASE=precise
-QEMU_TARGET=arm-softmmu
+if [ -z "$UBUNTU_RELEASE" ]; then
+  UBUNTU_RELEASE=trusty
+fi
+if [ -z "$UBUNTU_ARCH" ]; then
+  UBUNTU_ARCH=arm64
+fi
+
+case "$UBUNTU_RELEASE" in
+  precise)
+	;;
+  trusty)
+	;;
+  *)
+    echo "WARNING unknown arch: $UBUNTU_RELEASE"
+esac
+
+case "$UBUNTU_ARCH" in
+  armel)
+    export CROSS_COMPILE=arm-none-eabi-
+    TOOLCHAIN_URL=http://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-arm-none-eabi-4.9-2014.09_linux.tar.xz
+    LINUX_ARCH=arm
+    LINUX_IMAGE=zImage
+    QEMU_ARCH=arm
+    DEFCONFIG=vexpress_defconfig
+    CPU=cortex-a15
+    if [ "$UBUNTU_RELEASE" != "precise" ]; then
+      echo "armel only supported on precise"
+      exit 1
+    fi
+    ;;
+  armhf)
+    export CROSS_COMPILE=arm-linux-gnueabihf-
+    TOOLCHAIN_URL=http://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux.tar.xz
+    LINUX_ARCH=arm
+    LINUX_IMAGE=zImage
+    QEMU_ARCH=arm
+    DEFCONFIG=vexpress_defconfig
+    CPU=cortex-a15
+    ;;
+  arm64)
+    export CROSS_COMPILE=aarch64-linux-gnu-
+    TOOLCHAIN_URL=http://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-aarch64-linux-gnu-4.9-2014.09_linux.tar.xz
+    LINUX_ARCH=arm64
+    LINUX_IMAGE=Image.gz
+    QEMU_ARCH=aarch64
+    DEFCONFIG=defconfig
+    CPU=cortex-a57
+    ;;
+  *)
+    echo "unknown arch: $UBUNTU_ARCH"
+    exit 1
+esac
+
+
 PASSWORD=lldb4life
 USER_LOGIN=linaro
-TOOLCHAIN_URL=http://releases.linaro.org/14.09/components/toolchain/binaries/gcc-linaro-arm-none-eabi-4.9-2014.09_linux.tar.xz
 
 # composite variables
-SYS_ROOT=$RELEASE-$ARCHEX
-WORK_DIR=$HOME/$SYS_ROOT-work
+SYS_ROOT=$UBUNTU_RELEASE-$UBUNTU_ARCH
+WORK_DIR=$VMHOME/$SYS_ROOT-work
 IMG_FILE=$SYS_ROOT.img
 TOOLCHAIN_DIR=$WORK_DIR/toolchain
 export PATH=$TOOLCHAIN_DIR/bin:$PATH
@@ -29,7 +100,7 @@ export PATH=$TOOLCHAIN_DIR/bin:$PATH
 # clean up any tmp folders
 mkdir -p $WORK_DIR
 cd $WORK_DIR
-sudo rm -rf $WORK_DIR/tmp tmp.img || true
+sudo rm -rf $WORK_DIR/tmp $WORK_DIR/tmp.img || true
 
 function build_qemu {
   if [ -d $WORK_DIR/qemu ]; then
@@ -39,8 +110,8 @@ function build_qemu {
 
   cd $WORK_DIR
   git clone git://git.qemu.org/qemu.git tmp && cd tmp
-  ./configure --target-list=$QEMU_TARGET
-  make -j32
+  ./configure --target-list=$QEMU_ARCH-softmmu
+  make -j$NCPU
 
   cd $WORK_DIR
   mv tmp qemu
@@ -55,9 +126,9 @@ function download_packages {
   cd $WORK_DIR
 
   sudo mkdir tmp
-  sudo qemu-debootstrap --arch=$ARCHEX $RELEASE tmp/
-  echo "deb http://ports.ubuntu.com/ubuntu-ports $RELEASE main restricted universe" > sources.list
-  echo "deb-src http://ports.ubuntu.com/ubuntu-ports $RELEASE main restricted universe" >> sources.list
+  sudo qemu-debootstrap --arch=$UBUNTU_ARCH $UBUNTU_RELEASE tmp/
+  echo "deb http://ports.ubuntu.com/ubuntu-ports $UBUNTU_RELEASE main restricted universe" > sources.list
+  echo "deb-src http://ports.ubuntu.com/ubuntu-ports $UBUNTU_RELEASE main restricted universe" >> sources.list
   sudo mv sources.list tmp/etc/apt/
   sudo chroot tmp /usr/bin/apt-get -y install openssh-server
   sudo mv tmp $SYS_ROOT-clean
@@ -84,7 +155,7 @@ function build_sysroot {
 
   # enable passwordless ssh to this guest
   sudo mkdir tmp/home/$USER_LOGIN/.ssh
-  sudo cp $HOME/.ssh/id_rsa.pub tmp/home/$USER_LOGIN/.ssh/
+  sudo cp $HOME/.ssh/id_rsa.pub tmp/home/$USER_LOGIN/.ssh/authorized_keys
   sudo chroot tmp chown -R $USER_LOGIN:$USER_LOGIN /home/$USER_LOGIN/.ssh/
 
   # set passwords
@@ -139,9 +210,10 @@ function build_linux {
 
   git clone https://github.com/torvalds/linux.git tmp
   cd tmp
-  #git checkout v3.7 # or some other version
+  git checkout v3.19 # current stable
 
-  make vexpress_defconfig
+  export ARCH=$LINUX_ARCH
+  make $DEFCONFIG
   ./scripts/config -e CONFIG_LBDAF
   ./scripts/config -e CONFIG_VIRTIO_BLK
   ./scripts/config -e CONFIG_SCSI_VIRTIO
@@ -152,7 +224,7 @@ function build_linux {
   ./scripts/config -e CONFIG_VIRTIO_MMIO
   ./scripts/config -e CONFIG_DEVTMPFS_MOUNT
   yes '' | make oldconfig
-  make -j32
+  make -j$NCPU
 
   cd $WORK_DIR
   
@@ -168,11 +240,11 @@ build_image
 
 cd $WORK_DIR
 
-qemu/$QEMU_TARGET/qemu-system-$ARCH \
-  -cpu cortex-a15 \
+qemu/$QEMU_ARCH-softmmu/qemu-system-$QEMU_ARCH \
+  -cpu $CPU \
   -machine type=virt \
   -m 2048 \
-  -kernel linux/arch/$ARCH/boot/zImage \
+  -kernel linux/arch/$LINUX_ARCH/boot/$LINUX_IMAGE \
   -append 'console=ttyAMA0 root=/dev/vda rw' \
   -serial mon:stdio \
   -display none \
